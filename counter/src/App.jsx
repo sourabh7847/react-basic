a499647b87a515ff52538b87439714110f13794c
sourabh7847 <flank.valish@gmail.com>
2025-04-18 00:39:42 +0530

    counter code modified

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -6,27 +6,41 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   // let counter = 20;
-  const incValue = () => {
-    // console.log("value added", counter);
+  // const incValue = () => {
+  //   // console.log("value added", counter);
 
-    setCount(count + 1);
-  };
-  const decValue = () => {
-    // console.log("value added", counter);
-    if (count <= 0) {
-      alert("Count value should not go in negative value");
-      return 0;
+  //   setCount(count + 1);
+  // };
+  // const decValue = () => {
+  //   // console.log("value added", counter);
+  //   if (count <= 0) {
+  //     alert("Count value should not go in negative value");
+  //     return 0;
+  //   }
+  //   setCount(count - 1);
+  // };
+
+  const onCounterClick = (type) =>  {
+    if(type == 'inc') {
+      setCount(count + 1);
+    } else {
+      if(count <= 0) {
+        alert("not allowed")
+      } else {
+        setCount(count - 1);
+      }
     }
-    setCount(count - 1);
-  };
+  }
+
+
   return (
     <>
       <h1>chai aur code</h1>
       <h2>Counter value: {count}</h2>
 
-      <button onClick={incValue}>increase value</button>
+      <button onClick={() => onCounterClick("inc")}>increase value</button>
       <br />
-      <button onClick={decValue}>decrease value</button>
+      <button onClick={() =>onCounterClick("dec")}>decrease value</button>
     </>
   );
 }
