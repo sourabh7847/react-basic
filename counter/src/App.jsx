c82ff9ab48528dc8a9018ef58c254ceaedd9a068
sourabh7847 <flank.valish@gmail.com>
2025-04-19 01:05:50 +0530

    added form section

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -5,6 +5,10 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setdata] = useState({
+    name: "",
+    age: 0,
+  });
   // let counter = 20;
   // const incValue = () => {
   //   // console.log("value added", counter);
@@ -20,27 +24,54 @@ function App() {
   //   setCount(count - 1);
   // };
 
-  const onCounterClick = (type) =>  {
-    if(type == 'inc') {
+  const onCounterClick = (type) => {
+    if (type == "inc") {
       setCount(count + 1);
     } else {
-      if(count <= 0) {
-        alert("not allowed")
+      if (count <= 0) {
+        alert("not allowed");
       } else {
         setCount(count - 1);
       }
     }
-  }
+  };
 
+  const handleInputClick = (key, val)=>{
+    setdata({ ...data, [key]: val })
+  }
 
   return (
     <>
       <h1>chai aur code</h1>
       <h2>Counter value: {count}</h2>
 
-      <button onClick={() => onCounterClick("inc")}>increase value</button>
+      <button onClick={() => onCounterClick("inc")}>
+        increase value
+      </button>
       <br />
-      <button onClick={() =>onCounterClick("dec")}>decrease value</button>
+      <button onClick={() => onCounterClick("dec")}>
+        decrease value
+      </button>
+      <br />
+      <label>
+        Name:{" "}
+        <input
+          value={data.name}
+          onChange={(e) =>
+            handleInputClick("",e.target.value)
+          }
+        />
+        <br />
+        Age:{" "}
+        <input
+          value={data.age}
+          onChange={(e) => handleInputClick("age",e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={() => console.log(data)}>
+        submit
+      </button>
     </>
   );
 }
